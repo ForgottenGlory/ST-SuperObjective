488cea2e5ab9826bce985e7bd1ed9a940355fce1
wolf <wolf@kawaiiwolf.com>
2025-10-19 17:54:24 -0700

    Bug Fix Testing

--- index.js
+++ index.js
@@ -1919,7 +1919,7 @@ function doPopout(e) {
     } else {
         console.debug('saw existing popout, removing');
         if ($('objectiveExtensionDrawerContents').length === 0) {
-            $(target).parent().parent().parent().find('.inline-drawer-content').append(objectivePopoutHTML);
+            $(target).parent().parent().parent().find('.inline-drawer-content').html(objectivePopoutHTML);
             loadSettings();
         }
         else
