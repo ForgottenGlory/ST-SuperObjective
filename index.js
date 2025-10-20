5562f42452839c2ed672df441431bf3817bf0f94
wolf <wolf@kawaiiwolf.com>
2025-10-19 20:48:58 -0700

    Bug Fix Testing

--- index.js
+++ index.js
@@ -1891,6 +1891,55 @@ function doPopout(e) {
     const target = e.target;
 
     //repurposes the zoomed avatar template to server as a floating div
+    if ($('#objectiveExtensionPopout').length === 0) {
+        console.debug('did not see popout yet, creating');
+        const originalHTMLClone = $(target).parent().parent().parent().find('.inline-drawer-content').html();
+        const originalElement = $(target).parent().parent().parent().find('.inline-drawer-content');
+        const template = $('#zoomed_avatar_template').html();
+        const controlBarHtml = `<div class="panelControlBar flex-container">
+        <div id="objectiveExtensionPopoutheader" class="fa-solid fa-grip drag-grabber hoverglow"></div>
+        <div id="objectiveExtensionPopoutClose" class="fa-solid fa-circle-xmark hoverglow dragClose"></div>
+        </div>`;
+        const newElement = $(template);
+        newElement
+            .attr('id', 'objectiveExtensionPopout')
+            .removeClass('zoomed_avatar')
+            .addClass('draggable')
+            .empty();
+        originalElement.html('<div class="flex-container alignitemscenter justifyCenter wide100p"><small>Currently popped out</small></div>');
+        newElement.append(controlBarHtml).append(originalHTMLClone);
+        $('#movingDivs').append(newElement);
+        $('#objectiveExtensionDrawerContents').addClass('scrollY');
+        loadSettings();
+        loadMovingUIState();
+
+        $('#objectiveExtensionPopout').css('display', 'flex').fadeIn(animation_duration);
+        dragElement(newElement);
+
+        var objectivePopoutHTML = $('#objectiveExtensionDrawerContents');
+
+        //setup listener for close button to restore extensions menu
+        $('#objectiveExtensionPopoutClose').off('click').on('click', function () {
+            $('#objectiveExtensionDrawerContents').removeClass('scrollY');
+            objectivePopoutHTML = $('#objectiveExtensionDrawerContents');
+            $('#objectiveExtensionPopout').fadeOut(animation_duration, () => {
+                originalElement.empty();
+                originalElement.append(objectivePopoutHTML);
+                $('#objectiveExtensionPopout').remove();
+            });
+            loadSettings();
+        });
+    } else {
+        console.debug('saw existing popout, removing');
+        $('#objectiveExtensionPopout').fadeOut(animation_duration, () => { $('#objectiveExtensionPopoutClose').trigger('click'); });
+    }
+}
+
+/*
+function doPopout(e) {
+    const target = e.target;
+
+    //repurposes the zoomed avatar template to server as a floating div
     if ($('#objectiveExtensionPopout').length === 0 && $('#objectiveExtensionDrawerContents').length !== 0) {
         console.debug('did not see popout yet, creating');
         const originalHTMLClone = $(target).parent().parent().parent().find('.inline-drawer-content').html();
@@ -1944,6 +1993,7 @@ function doPopout(e) {
         $('#objectiveExtensionPopout').fadeOut(animation_duration, () => { $('#objectiveExtensionPopoutClose').trigger('click'); });
     }
 }
+*/
 
 // Add template management UI
 function onManageTemplatesClick() {
